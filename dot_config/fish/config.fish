3eaae81b3750a9d717d63e25f36db149baa7d5ba
T. Yang <yangtaotao0524@gmail.com>
2026-01-13 21:54:26 +0100

    init: add initial configs - init: add .gitconfig - init: add .ssh/config - init: add fcitx5 rime - init: add starship - init: add alacritty - init: add ghostty - init: add btop - init: add fastfetch - init: add fish - init: add nvim - init: add solaar

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,53 @@
+# fish greetings to empty
+set -g fish_greeting ""
+
+# env
+if test -f ~/.fish_profile
+    source ~/.fish_profile
+end
+
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        ser -p PATH ~/.local/bin
+    end
+end
+
+# module init
+starship init fish | source
+zoxide init fish | source
+fzf --fish | source
+
+# fetch
+fastfetch
+
+# ssh management
+keychain --quiet --eval tyang@nightfall | source
+
+# alias
+## allegro 
+alias mount-miscanti 'mkdir -p ~/remote-miscanti && sshfs -o follow-symlinks tyang@miscanti:/almastorage/allegro/home/tyang ~/remote-miscanti'
+alias umount-miscanti 'fusermount3 -u ~/remote-miscanti 2>/dev/null && rmdir ~/remote-miscanti 2>/dev/null'
+## eza - ls replace
+alias ls 'eza -al --color=always --group-directories-first --icons'
+alias la 'eza -a --color=always --group-directories-first --icons'
+alias ll 'eza -l --color=always --group-directories-first --icons'
+alias lt 'eza -aT --color=always --group-directories-first --icons'
+## cd
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+## common
+alias wget 'wget -c'
+alias grep 'grep --color=auto'
+alias fgrep 'fgrep --color=auto'
+alias egrep 'egrep --color=auto'
+## tar
+alias tarthis 'tar -acf'
+alias untarthis 'tar -zxvf'
+## additional
+alias hw 'hwinfo --short'
+alias big "expac -H M '%m\t%n' | sort -h | nl"
+alias rip "expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+alias update 'sudo pacman -Syu'
+alias clean 'sudo pacman -Rns (pacman -Qtdq)'
+alias jctl 'journalctl -p 3 -xb'
---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+-- custom
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
